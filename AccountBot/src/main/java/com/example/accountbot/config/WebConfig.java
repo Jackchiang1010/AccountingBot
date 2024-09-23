65e40b802243cb7f5d2a5815f40cf0cede29a06a
Jackchiang1010 <jackgg44109@gmail.com>
2024-09-23 10:44:26 +0800

    [fix] fix bug for FrontEnd call API 403 error

--- AccountBot/src/main/java/com/example/accountbot/config/WebConfig.java
+++ AccountBot/src/main/java/com/example/accountbot/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://jacktest.site", "https://*.ngrok-free.app")
+                .allowedOriginPatterns("https://jacktest.site", "https://*.ngrok-free.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
