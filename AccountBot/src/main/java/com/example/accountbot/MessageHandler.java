9ea8972dd14bad43264b5ac74be5bfc0847f09cf
Jackchiang1010 <jackgg44109@gmail.com>
2024-10-18 13:09:07 +0800

    [refactor] refactor MessangeHandler and application.properties

--- AccountBot/src/main/java/com/example/accountbot/MessageHandler.java
+++ AccountBot/src/main/java/com/example/accountbot/MessageHandler.java
@@ -71,16 +71,17 @@ public class MessageHandler {
         String userId = event.getSource().getUserId();
         String receivedText = event.getMessage().getText();
 
-//        lastUserMessage = receivedText;
-
         try {
 
-            if (receivedText != null && (receivedText.startsWith("支出報表") || receivedText.startsWith("收入報表"))) {
+            if (receivedText == null || receivedText.isEmpty()) {
+                log.info("空的訊息內容");
+                return;
+            }
 
-                // 根據開頭決定第一個參數的值
+            if (receivedText.startsWith("支出報表") || receivedText.startsWith("收入報表")) {
+
                 int type = receivedText.startsWith("收入報表") ? 0 : 1;
 
-                // 根據使用者輸入更換時間範圍
                 String timePeriod;
                 if (receivedText.contains("昨日")) {
                     timePeriod = "yesterday";
@@ -91,11 +92,10 @@ public class MessageHandler {
                 } else if (receivedText.contains("半年")) {
                     timePeriod = "halfYear";
                 } else {
-                    timePeriod = "month"; // 預設為 "本月"
+                    timePeriod = "month";
                 }
 
                 String outputFilePath = "src/main/resources/static/images/transactionPieChart.png";
-                // 生成圓餅圖
                 String imagePath = chartGenerateService.generatePieChart(type, timePeriod, outputFilePath, userId);
 
                 if (imagePath != null) {
@@ -104,7 +104,6 @@ public class MessageHandler {
                     log.info("圖片生成失敗");
                 }
 
-                // 根據開頭動態設置 displayText
                 String displayTextPrefix = receivedText.startsWith("收入報表") ? "收入報表" : "支出報表";
 
                 String flexMessageJson = String.format("""
@@ -181,12 +180,8 @@ public class MessageHandler {
 
                 JSONObject flexMessageJsonObject = new JSONObject(flexMessageJson);
 
-                String s3Key = "images/transaction/" + UUID.randomUUID() + ".png"; // 在 S3 上的路徑和檔案名稱
+                String s3Key = "images/transaction/" + UUID.randomUUID() + ".png";
                 String imageUrl = uploadImageToS3(imagePath, s3Key);
-
-//                flexMessageJsonObject.getJSONObject("hero")
-//                        .getJSONObject("action")
-//                        .put("uri", imageUrl);
 
                 flexMessageJsonObject.getJSONObject("hero")
                         .put("url", imageUrl);
@@ -202,7 +197,7 @@ public class MessageHandler {
                 }
             }
 
-            else if (receivedText != null && receivedText.matches("\\$.*")) {
+            else if (receivedText.matches("\\$.*")) {
                 try {
 
                     if (!validateCost(receivedText, userId)) {
@@ -214,7 +209,6 @@ public class MessageHandler {
                     Map<String, Object> expenseCategoryMap = categoryService.get(1, "all", userId);
                     List<UpdateCategoryDto> expenseCategories = (List<UpdateCategoryDto>) expenseCategoryMap.get("data");
 
-                    // 動態生成 FlexMessage 的分類按鈕 JSON 內容
                     StringBuilder buttonContents = new StringBuilder();
                     int buttonCount = 0;
 
@@ -266,20 +260,19 @@ public class MessageHandler {
 
                     String flexMessageJson = """
         {
-          "type": "bubble",
-          "size": "mega",
-          "body": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-              %s
-            ],
-            "spacing": "sm"
-          }
+            "type": "bubble",
+            "size": "mega",
+            "body": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                    %s
+                ],
+                "spacing": "sm"
+            }
         }
         """.formatted(buttonContents.toString());
 
-                    // 解析 JSON 並發送 FlexMessage
                     FlexContainer flexContainer = objectMapper.readValue(flexMessageJson, FlexContainer.class);
                     FlexMessage flexMessage = new FlexMessage("記支出", flexContainer);
 
@@ -291,7 +284,7 @@ public class MessageHandler {
                 }
             }
 
-            else if (receivedText != null && receivedText.matches("\\+.*")) {
+            else if (receivedText.matches("\\+.*")) {
                 try {
 
                     if (!validateCost(receivedText, userId)) {
@@ -303,7 +296,6 @@ public class MessageHandler {
                     Map<String, Object> incomeCategoryMap = categoryService.get(0, "all", userId);
                     List<UpdateCategoryDto> incomeCategories = (List<UpdateCategoryDto>) incomeCategoryMap.get("data");
 
-                    // 動態生成 FlexMessage 的分類按鈕 JSON 內容
                     StringBuilder buttonContents = new StringBuilder();
                     int buttonCount = 0;
 
@@ -355,20 +347,19 @@ public class MessageHandler {
 
                     String flexMessageJson = """
         {
-          "type": "bubble",
-          "size": "mega",
-          "body": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-              %s
-            ],
-            "spacing": "sm"
-          }
+            "type": "bubble",
+            "size": "mega",
+            "body": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                    %s
+                ],
+                "spacing": "sm"
+            }
         }
         """.formatted(buttonContents.toString());
 
-                    // 解析 JSON 並發送 FlexMessage
                     FlexContainer flexContainer = objectMapper.readValue(flexMessageJson, FlexContainer.class);
                     FlexMessage flexMessage = new FlexMessage("記收入", flexContainer);
 
@@ -380,22 +371,19 @@ public class MessageHandler {
                 }
             }
 
-            else if ( receivedText != null && receivedText.matches("^刪除.*")){
-                // 解析輸入以取得類別和 ID
+            else if (receivedText.matches("^刪除.*")){
                 String[] parts = receivedText.split(":");
                 if (parts.length == 2) {
                     String type = receivedText.substring(2);
                     int transactionId = Integer.parseInt(parts[1].trim());
 
-                    // 呼叫 transactionService 刪除該筆資料
                     transactionService.delete(transactionId);
 
-                    // 回覆刪除成功訊息
                     sendLineMessage(userId, type + " 已成功刪除");
                 }
             }
 
-            else if (receivedText != null && receivedText.matches("^本月結餘.*")) {
+            else if (receivedText.matches("^本月結餘.*")) {
 
                 BalanceDto balanceDto = transactionService.balance(userId);
                 log.info("balanceDto : " + balanceDto);
@@ -412,7 +400,6 @@ public class MessageHandler {
                 TextMessage textMessage = new TextMessage(message);
 
                 String outputFilePath = "src/main/resources/static/images/balanceBarChart.png";
-                // 生成長條圖
                 String imagePath = chartGenerateService.generateBarChart(totalIncome, totalExpenses, netBalance, outputFilePath);
 
                 if (imagePath != null) {
@@ -425,30 +412,26 @@ public class MessageHandler {
 
                 String flexMessageJson = """
             {
-                 "type": "bubble",
-                 "size": "giga",
-                 "hero": {
-                   "type": "image",
-                   "size": "full",
-                   "aspectMode": "cover",
-                   "action": {
-                     "type": "uri",
-                     "uri": "https://jacktest.site/index.html"
-                   },
-                   "url": "%s.png",
-                   "aspectRatio": "20:13"
-                 }
-               }
+                "type": "bubble",
+                "size": "giga",
+                "hero": {
+                    "type": "image",
+                    "size": "full",
+                    "aspectMode": "cover",
+                    "action": {
+                        "type": "uri",
+                        "uri": "https://jacktest.site/index.html"
+                    },
+                    "url": "%s.png",
+                    "aspectRatio": "20:13"
+                }
+            }
         """;
 
                 JSONObject flexMessageJsonObject = new JSONObject(flexMessageJson);
 
-                String s3Key = "images/balance/" + UUID.randomUUID() + ".png"; // 在 S3 上的路徑和檔案名稱
+                String s3Key = "images/balance/" + UUID.randomUUID() + ".png";
                 String imageUrl = uploadImageToS3(imagePath, s3Key);
-
-//                flexMessageJsonObject.getJSONObject("hero")
-//                        .getJSONObject("action")
-//                        .put("uri", imageUrl);
 
                 flexMessageJsonObject.getJSONObject("hero")
                         .put("url", imageUrl);
@@ -470,10 +453,10 @@ public class MessageHandler {
 
             }
 
-            else if (receivedText != null && receivedText.matches("^匯出上月報表.*")) {
+            else if (receivedText.matches("^匯出上月報表.*")) {
 
                 ZoneId taipeiZone = ZoneId.of("Asia/Taipei");
-                LocalDate today = LocalDate.now(taipeiZone); // 取得台灣時間的今天日期
+                LocalDate today = LocalDate.now(taipeiZone);
                 LocalDate startDate = today.minusMonths(1).withDayOfMonth(1);
                 LocalDate endDate = today.minusMonths(1).with(TemporalAdjusters.lastDayOfMonth());
 
@@ -488,17 +471,15 @@ public class MessageHandler {
 
             }
 
-            else if (receivedText != null && receivedText.matches("^分析上月報表.*")) {
+            else if (receivedText.matches("^分析上月報表.*")) {
 
                 Map<String, Object> expense = transactionService.getTransaction(1, "all", "lastMonth", userId);
                 Map<String, Object> income = transactionService.getTransaction(0, "all", "lastMonth", userId);
 
                 Map<String, Object> response = aiService.getFeedback(expense, income);
 
-                // 取得 "data" 的內容
                 AIFeedbackDto data = (AIFeedbackDto) response.get("data");
 
-                // 分別提取分析和建議訊息
                 String analysisMessage = data.getAnalysis();
                 String adviceMessage = data.getAdvice();
 
@@ -507,7 +488,7 @@ public class MessageHandler {
 
             }
 
-            else if (receivedText != null && (receivedText.matches("^指令.*") || receivedText.matches("^教學.*"))) {
+            else if ((receivedText.matches("^指令.*") || receivedText.matches("^教學.*"))) {
                 String flexMessageJson = String.format("""
         {
             "type": "bubble",
@@ -667,7 +648,6 @@ public class MessageHandler {
 
     }
 
-    //記帳，flexMessage 內放 postback
     @EventMapping
     public void handlePostbackEvent(PostbackEvent event) {
         String userId = event.getSource().getUserId();
@@ -697,17 +677,14 @@ public class MessageHandler {
                 categoryTypes.put(categoryName, 0);
             }
 
-            // Check if the postbackData is in the map
             if (categoryMessages.containsKey(postbackData)) {
                 type = categoryTypes.get(postbackData);
                 String typeStr = "";
 
                 lastUserMessage = redisUtil.getDataFromCache(CACHE_KEY + userId);
 
-                // 預處理輸入字串：去除首尾空白並將多個空白字符替換為單個空格
                 String cleanedMessage = lastUserMessage.trim().replaceAll("\\s+", " ");
 
-                // 以空格分割字串，限制最多分成兩部分：金額和描述
                 String[] parts = cleanedMessage.substring(1).trim().split("\\s+", 2);
 
                 String amountStr = parts[0].trim();
@@ -725,7 +702,6 @@ public class MessageHandler {
                     budgetAlert(postbackData, userId, type);
                 }
 
-                // 使用 generateRecordImage 方法生成圖像
                 String imagePath = chartGenerateService.generateRecordImage(type, amount, postbackData, userId);
 
                 if (imagePath == null) {
@@ -733,8 +709,7 @@ public class MessageHandler {
                     return;
                 }
 
-                // 上傳圖片到 S3
-                String s3Key = "images/record/" + UUID.randomUUID() + ".png"; // 在 S3 上的路徑和檔案名稱
+                String s3Key = "images/record/" + UUID.randomUUID() + ".png";
                 String imageUrl = uploadImageToS3(imagePath, s3Key);
 
                 if (imageUrl == null) {
@@ -748,80 +723,78 @@ public class MessageHandler {
                     typeStr = "支出";
                 }
 
-                // 動態生成 FlexMessage 的 JSON 內容
                 String flexMessageJson = """
     {
-       "type": "bubble",
-       "size": "giga",
-       "hero": {
-         "type": "image",
-         "url": "%s",
-         "size": "full",
-         "aspectRatio": "2:1",
-         "aspectMode": "cover",
-         "action": {
-           "type": "uri",
-           "uri": "https://jacktest.site/index.html"
-         }
-       },
-       "body": {
-         "type": "box",
-         "layout": "horizontal",
-         "contents": [
-           {
-             "type": "button",
-             "action": {
-               "type": "uri",
-               "label": "編輯",
-               "uri": "https://jacktest.site/transactionDetail.html?lineUserId=%s&transactionId=%s"
-             },
-              "color": "#F7D486",
-              "style": "secondary",
-              "height": "md",
-              "margin": "md"
-           },
-           {
-             "type": "button",
-             "action": {
-               "type": "message",
-               "label": "刪除",
-               "text": "刪除%s ID:%s"
-             },
-              "color": "#F7D486",
-              "style": "secondary",
-              "height": "md",
-              "margin": "md"
-           },
-           {
-             "type": "button",
-             "action": {
-               "type": "message",
-               "label": "報表",
-               "text": "%s報表"
-             },
-              "color": "#F7D486",
-              "style": "secondary",
-              "height": "md",
-              "margin": "md"
-           },
-           {
-             "type": "button",
-             "action": {
-               "type": "message",
-               "label": "結餘",
-               "text": "本月結餘"
-             },
-              "color": "#F7D486",
-              "style": "secondary",
-              "height": "md",
-              "margin": "md"
-           }
-         ]
-       }
-     }
+        "type": "bubble",
+        "size": "giga",
+        "hero": {
+            "type": "image",
+            "url": "%s",
+            "size": "full",
+            "aspectRatio": "2:1",
+            "aspectMode": "cover",
+            "action": {
+            "type": "uri",
+            "uri": "https://jacktest.site/index.html"
+            }
+        },
+        "body": {
+            "type": "box",
+            "layout": "horizontal",
+            "contents": [
+            {
+                "type": "button",
+                "action": {
+                "type": "uri",
+                "label": "編輯",
+                "uri": "https://jacktest.site/transactionDetail.html?lineUserId=%s&transactionId=%s"
+                },
+                "color": "#F7D486",
+                "style": "secondary",
+                "height": "md",
+                "margin": "md"
+            },
+            {
+                "type": "button",
+                "action": {
+                "type": "message",
+                "label": "刪除",
+                "text": "刪除%s ID:%s"
+                },
+                "color": "#F7D486",
+                "style": "secondary",
+                "height": "md",
+                "margin": "md"
+            },
+            {
+                "type": "button",
+                "action": {
+                "type": "message",
+                "label": "報表",
+                "text": "%s報表"
+                },
+                "color": "#F7D486",
+                "style": "secondary",
+                "height": "md",
+                "margin": "md"
+            },
+            {
+                "type": "button",
+                "action": {
+                "type": "message",
+                "label": "結餘",
+                "text": "本月結餘"
+                },
+                "color": "#F7D486",
+                "style": "secondary",
+                "height": "md",
+                "margin": "md"
+            }
+            ]
+        }
+        }
 """.formatted(imageUrl, userId, transactionId, typeStr, transactionId, typeStr);
 
-// 解析 JSON 並發送 FlexMessage
                 FlexContainer flexContainer = objectMapper.readValue(flexMessageJson, FlexContainer.class);
                 FlexMessage flexMessage = new FlexMessage("記帳資訊", flexContainer);
 
@@ -829,7 +802,7 @@ public class MessageHandler {
             }
 
         } catch (InterruptedException e) {
-            Thread.currentThread().interrupt(); // 恢復中斷狀態
+            Thread.currentThread().interrupt();
             e.printStackTrace();
         } catch (ExecutionException e) {
             e.printStackTrace();
@@ -870,8 +843,6 @@ public class MessageHandler {
                 sendLineMessage(lineUserId, "已超出 " + category + " 預算");
             } else if ((double)totalCost / budgetPrice >= 0.5) {
                 sendLineMessage(lineUserId, "已超出 " + category + " 一半預算");
-            } else {
-                //TODO 未超出一半預算
             }
         }catch (Exception e) {
             e.printStackTrace();
@@ -888,14 +859,9 @@ public class MessageHandler {
     }
 
     public String dateToString(LocalDate date) {
-        ZoneId taipeiZone = ZoneId.of("Asia/Taipei");
-        LocalDate today = LocalDate.now(taipeiZone); // 取得台灣時間的今天日期
-        // 定義日期格式
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
-        // 將 LocalDate 轉換為 String
-        String dateStr = date.format(formatter);
-        return dateStr;
+        return date.format(formatter);
     }
 
     public boolean validateCost(String receivedText, String userId) {
